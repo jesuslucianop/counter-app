a7004f874ddbb208d98341e7fe49f2ae81c0da31
jesus luciano <luciano-perlata@hotmail.com>
2019-07-12 11:31:09 -0400

    Usando el manejo de componentes por primera vez

---
+++ src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return <h1>hola mundo</h1>;
+    }
+}
+ 
+export default Counter;
