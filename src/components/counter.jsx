67191db8685573bc3852ce46c916cf7f2a09cb89
jesus luciano <luciano-perlata@hotmail.com>
2019-07-15 16:47:04 -0400

    Se agrego la funcion que incrementa el contador, se agrego la funcion que drecrementa, utilizamos los constructores  ya que sin ellos el evento onclick nos da undefined para ello sustituimos el construcotr en caso de que no vayamos a inicializar nada por funciones de flecha, tambien el uso de condiciones

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 class Counter extends Component {
    state = {
-        count:1,
+        count:0,
         address:{
             street:'',
         }
 
    };
+ 
+ /*  constructor()
+   {
+       super();
+       this.Incrementn = this.Incrementn.bind(this);
+   }*/
+   Incrementn = ()=>
+   {
+       console.log("Incrementando "+this);
+       this.setState({count:this.state.count + 1});
+   }
+   Decrement =  ()=>
+   {
+    this.setState({count:this.state.count - 1});
+   }
+   Showdrement = ()=>
+   {
+    let pruea = this.state.count >0 ?
+    <button
+    onClick={this.Decrement}
+    className="btn btn-danger btn-sm"
+    >
+        Decrement
+        </button>:" " ;
+    return pruea;
+   }
     render() { 
         React.createElement('h1');
         return <div>
-        <span>{this.formatCount()}</span>
-            <button>increment</button>
-        
+        <span className={this.Getbadgeclasses()}>{this.formatCount()}</span>
+            <button onClick={this.Incrementn}
+           className="btn btn-secondary btn-sm"
+           >
+                increment
+            </button>
+            <span>{this.Showdrement()}</span>
+     
             </div>;
     }
     formatCount()
@@ -20,6 +51,13 @@ class Counter extends Component {
      const {count}= this.state;
      return count ===0 ? "zero":count;
  }
+    Getbadgeclasses()
+    {
+        let classes = "badge m-2 badge-";
+        classes+= this.state.count ===0 ?"warning":"primary";
+        return classes;
+    }
+ 
 }
  
 export default Counter;
